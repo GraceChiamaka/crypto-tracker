bee31981d8df6b4ec8cbac355d33cd17e147a5fa
GraceieC <grace.chiamaka@zingly.ai>
2025-05-02 23:06:38 +0100

    feature-market: add updates to display market list of coins

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,18 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { AntThemeProvider } from "./theme/theme-provider.tsx";
+import { Provider } from "react-redux";
+import { store } from "./store/index.ts";
 import "./index.css";
 
 import Router from "./routes/routes.tsx";
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
-        <AntThemeProvider>
-            <Router />
-        </AntThemeProvider>
+        <Provider store={store}>
+            <AntThemeProvider>
+                <Router />
+            </AntThemeProvider>
+        </Provider>
     </StrictMode>,
 );
